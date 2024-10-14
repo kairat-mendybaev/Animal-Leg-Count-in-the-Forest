a4ae5994cff239665fa40bae69d211c84b537dfe
Kairat Mendybaev <113317646+kairat-mendybaev@users.noreply.github.com>
2024-10-13 22:48:45 -0700

    Update AnimalLegCount.java

--- AnimalLegCount.java
+++ AnimalLegCount.java
@@ -1,26 +1,46 @@
-
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class AnimalLegCount {
-    public static void main(String[] args) {
+
+    // List of animals with four legs
+    private static final List<String> fourLeggedAnimals = Arrays.asList("lion", "deer", "elephant", "horse", "dog", "cat");
+
+    // Method to get animal input from the user
+    public static List<String> getAnimalInput() {
         Scanner scan = new Scanner(System.in);
+        List<String> inputAnimals = new ArrayList<>();
+
         System.out.println("Enter any 5 animals from the list: 'lion', 'deer', 'elephant', 'horse', 'dog', 'cat', 'monkey', 'parrot', 'ostrich', 'snake', 'worm', 'spider', 'ant', 'centipede'");
 
-        ArrayList<String> arrayOfAnimals = new ArrayList<>();
-        ArrayList<String> fourLegsAnimals = new ArrayList<>(Arrays.asList("lion", "deer", "elephant", "horse", "dog", "cat"));
-
         for (int i = 0; i < 5; i++) {
-            arrayOfAnimals.add(scan.next());
+            inputAnimals.add(scan.next());
         }
 
-        System.out.println("Your list: " + arrayOfAnimals);
-
-        arrayOfAnimals.retainAll(fourLegsAnimals);
-        System.out.println("Only " + arrayOfAnimals + " have(s) 4 legs");
-        System.out.println(arrayOfAnimals.size());
-       }
-
+        return inputAnimals;
     }
+
+    // Method to filter animals with four legs
+    public static List<String> filterFourLeggedAnimals(List<String> animals) {
+        List<String> fourLeggedList = new ArrayList<>(animals);
+        fourLeggedList.retainAll(fourLeggedAnimals); // Retain only animals with four legs
+        return fourLeggedList;
+    }
+
+    // Method to count animals with four legs
+    public static int countFourLeggedAnimals(List<String> animals) {
+        return filterFourLeggedAnimals(animals).size();
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        List<String> animals = getAnimalInput();
+        System.out.println("Your list: " + animals);
+
+        List<String> fourLegged = filterFourLeggedAnimals(animals);
+        System.out.println("Only " + fourLegged + " have(s) 4 legs");
+        System.out.println("Count of animals with 4 legs: " + fourLegged.size());
+    }
+}
